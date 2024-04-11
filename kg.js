4d2f622767eccec8accfda0caa369df425eca0a7
Arnab Satpati <119133596+Arnab-satpati@users.noreply.github.com>
2024-04-11 16:38:27 +0530

    Add files via upload

---
+++ kg.js
@@ -0,0 +1,11 @@
+// var sidemenu=document.getElementById("sidemenu");
+//         function openmenu(){
+//             sidemenu.style.right="0";
+//         }
+//         function closemenu(){
+//             sidemenu.style.right="-9.5rem";
+//         }
+function onclickMenu(){
+    document.getElementById("menu").classList.toggle("icon");
+    document.getElementById("nav2").classList.toggle("change");
+}
